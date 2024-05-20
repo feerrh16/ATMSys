8055b945fed6116cb5420b35e9366713d83f19b4
MistersGentleman33 <88989163+MistersGentleman33@users.noreply.github.com>
2024-05-19 22:13:04 -0600

    Update pago.py
    Update Pago con Tarjeta

---
+++ Modulos/pago.py
@@ -0,0 +1,8 @@
+def pagar_tarjeta(usuario):
+    print("Pago de tarjeta de crédito")
+    cantidad = int(input("Ingrese la cantidad a pagar: "))
+    # Aquí se debería actualizar el saldo y registrar el pago en la base de datos
+    print(f"Pago de tarjeta de crédito de ${cantidad} exitoso")
+    print("Recibo generado")
+
+# En esta seccion se sigue con la logica de la funcion pagar_tarjeta()
