638528e42632ca55f9262993730a23ba007d4f21
Sergio Fernando Rodríguez Hernández <s.f.r.h1612@gmail.com>
2024-05-23 15:07:15 -0600

    Sin cambios importantes

--- src/Cajero.py
+++ src/Cajero.py
@@ -1,8 +1,5 @@
 import sys
-sys.path.append('ATMSys-1/src/DAO')
-sys.path.append('ATMSys-1/src/db_management')
-sys.path.append('ATMSys-1/src/function_models')
-sys.path.append('ATMSys-1/src/security')
+from CajeroDAO import *
 from logger_base import *
 from cifrado import *
 
@@ -40,4 +37,5 @@ if __name__ == '__main__':
     log.debug(cajero1)
 
     cajero2 = Cajero(11, 34875.65)
+    CajeroDAO.insertar(cajero2)
     log.debug(cajero2)
